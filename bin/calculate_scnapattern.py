2e37a33fee34c732cf308071f25409b7ff251136
Luca Beltrame <lbeltrame@kde.org>
2024-02-13 14:41:45 +0100

    Spice up a bit the calculator

--- bin/calculate_scnapattern.py
+++ bin/calculate_scnapattern.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from pathlib import Path
 from typing import Literal
 
 from sympy import im
@@ -36,8 +37,32 @@ def get_chromosomal_arm_lengths(genome: str="hg38") -> pd.DataFrame:
     return table
 
 
+def ucsc_to_ncbi(dataframe: pd.DataFrame) -> pd.DataFrame:
+
+    result = dataframe.transform_column("chromosome",
+                                        lambda x: x.str.replace, "chr", "",
+                                        elementwise=False)
+
+    return result
+
+def ncbi_to_ucsc(dataframe: pd.DataFrame) -> pd.DataFrame:
+
+    result = (
+        dataframe
+        .astype({"chromosome": "str"})
+        .transform_column("chromosome", lambda x: x.radd("chr"),
+                          elementwise=False)
+    )
+
+    return result
+
+
 def classifier(row) -> Literal['S', 'U', 'HU'] | None:
     cnb = row.cnb
+
+    if pd.isnull(cnb): # No CNB calculated, we assume stable
+        return "S"
+
     length = row.normalized_length
     if length > 0.95 and cnb < 2.5:
         return "S"
@@ -62,32 +87,76 @@ def adjust_call(value: int, ploidy: int) -> float:
     return adjusted
 
 
+def harmonize_columns(
+    dataframe: pd.DataFrame,
+        format: Literal['ascat_sc', 'ichorcna', 'ace']) -> pd.DataFrame:
+
+    match format:
+        case "ascat_sc":
+            dataframe = dataframe.rename(
+                columns={"total_copy_number": "absolute_copy_number"}
+            )
+        case "ace":
+            dataframe = dataframe.rename(
+                columns={"copies": "absolute_copy_number"}
+            )
+        case "ichorcna":
+            dataframe = dataframe.rename(
+                columns={"copy.number": "absolute_copy_number",
+                         "chr": "chromosome"}
+            )
+        case _:
+            raise ValueError(f"Unsupported file format {format}")
+
+    return dataframe
+
 def normalized_scna_length(absolute_calls: pd.DataFrame,
-                          arm_data: pd.DataFrame) -> pd.DataFrame:
+                           arm_data: pd.DataFrame) -> pd.DataFrame:
     # Convert the dataframes to PyRanges objects
     calls_pr = pr.PyRanges(absolute_calls.rename(
         columns={"chromosome": "Chromosome", "start": "Start", "end": "End"}))
-    arms_pr = pr.PyRanges(arm_data)
+    arms_pr = pr.PyRanges(arm_data.rename(
+        columns={"chrom": "Chromosome", "chromStart": "Start",
+                 "chromEnd": "End", "arm": "Name"}))
 
     # Perform intersection
     intersected = calls_pr.join(arms_pr, how='left')
 
+    # Calculate total chromosome lengths from arm_data
+    chromosome_lengths = arm_data.groupby('Chromosome', as_index=False).agg(
+        TotalLength=('End', 'max'))
+    chromosome_lengths.rename(columns={'chrom': 'Chromosome'}, inplace=True)
+
     # Calculate normalized length
     intersected_df = intersected.df
-    intersected_df['new_start'] = intersected_df[['Start', 'Start_b']].max(axis=1)
+    intersected_df['new_start'] = intersected_df[['Start', 'Start_b']].max(
+        axis=1)
     intersected_df['new_end'] = intersected_df[['End', 'End_b']].min(axis=1)
-    intersected_df['length'] = intersected_df['new_end'] - intersected_df['new_start']
+    intersected_df['length'] = (intersected_df['new_end'] -
+                                intersected_df['new_start'])
     intersected_df['normalized_length'] = intersected_df['length'] / intersected_df['ArmLength']
 
-    # Clean up the DataFrame
-    intersected_df = intersected_df.rename(
-        columns={"Name": "arm", "ArmLength": "arm_length"})
-    intersected_df = intersected_df.select_columns("Chromosome", "Start", "End",
-                                                   "sample", "arm",
-                                                   "arm_length",
-                                                   "normalized_length").clean_names()
+    # Merge with chromosome lengths to get the total length for normalization
+    intersected_df = pd.merge(intersected_df, chromosome_lengths,
+                             on='Chromosome', how='left')
 
-    return intersected_df
+    # Normalization: check if the segment spans both arms. If so,
+    # normalize over the total chromosome length.
+    intersected_df['normalized_length'] = intersected_df.apply(
+        lambda x: x['length'] / x['TotalLength']
+       if (x['Start'] <= x['Start_b'] and x['End'] >= x['End_b'])
+       else x['length'] / x['ArmLength'], axis=1)
+
+    # Clean up the DataFrame using pyjanitor to standardize column names
+    cleaned_df = (
+        intersected_df
+        .rename(columns={"Name": "arm", "ArmLength": "arm_length"})
+        .select_columns(['Chromosome', 'Start', 'End', 'sample', 'arm',
+                         'arm_length', 'normalized_length'])
+        .clean_names()
+    )
+
+    return cleaned_df
 
 
 def call_patterns(segments: pd.DataFrame, arm_data: pd.DataFrame,
@@ -134,7 +203,10 @@ def main():
     parser.add_argument("--ploidy", type=int, help="Ploidy of the sample",
                         default=2)
     parser.add_argument("--genome", choices=("hg19", "hg38"), default="hg38")
-    parser.add_argument("--file-format", choices=("ascat_sc", "ichorcna"))
+    parser.add_argument("--file-format", choices=("ascat_sc", "ichorcna",
+                                                  "ace"))
+    parser.add_argument("--genome-style", choices=("ucsc", "ncbi"),
+                        default="ucsc")
     parser.add_argument("source", help="Source segment file")
     parser.add_argument("destination", help="Destination to save to")
     parser.add_argument('--version', action='version',
@@ -142,7 +214,21 @@ def main():
 
     options = parser.parse_args()
 
-    source = pd.read_table(options.source)
+    source = pd.read_table(options.source).clean_names().astype(
+        {"chromosome": "str"}
+    )
+
+    if options.genome_style == "ucsc" and not source.chromosome.str.startswith("chr").all():
+        source = ncbi_to_ucsc(source)
+
+    if options.genome_style == "ncbi"and not source.chromosome.str.startswith("chr").any():
+        source = ucsc_to_ncbi(source)
+
+    if options.file_format == "ace":
+        source["sample"] = Path(options.source).stem
+
+
+    source = harmonize_columns(source, options.file_format)
 
     source = source.assign(length=source.end.sub(source.start))
 
